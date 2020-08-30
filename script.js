17f2990bab70d2345f0e52a5d8d9d9cffb1cf920
Yuta-0801 <70415559+Yuta-0801@users.noreply.github.com>
2020-08-30 17:25:37 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+$(function(){
+
+    let value=true;
+
+    $(".slick-1").slick({
+        speed: 1000,
+        autoplay: value,
+        autoplaySpeed:1000,
+        dots:true,
+        vertical:false,
+    });
+    $("body").fadeIn();
+
+
+
+    
+});
